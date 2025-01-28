742e3681b2cc7d450128e79b9f6fdb91e3e70fb5
Artem Chernenko <artem.chernenko@betterme.world>
2025-01-28 19:14:30 +0200

    add env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "log"
+import (
+	"github.com/joho/godotenv"
+	"github.com/timmy1496/social/internal/env"
+	"log"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":8077",
+		addr: env.GetString("ADDR", ":8077"),
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/timmy1496/social
 
 go 1.22.1
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)

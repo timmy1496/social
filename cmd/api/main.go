086db7cb32b0d481b0ba789b879248534f0384ef
Artem Chernenko <artem.chernenko@betterme.world>
2025-03-21 17:20:11 +0200

    config db connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/joho/godotenv"
+	"github.com/timmy1496/social/internal/db"
 	"github.com/timmy1496/social/internal/env"
 	"github.com/timmy1496/social/internal/store"
 	"log"
@@ -15,9 +16,29 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8077"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	storage := store.NewStorage(nil)
+	dbConn, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer dbConn.Close()
+	log.Println("database connection pool established")
+
+	storage := store.NewStorage(dbConn)
 
 	app := &application{
 		config:  cfg,

0c43ecd9c65c8f5a32dccb3abb095c43845ed198
Artem Chernenko <artem.chernenko@betterme.world>
2025-04-28 17:57:32 +0300

    add get post

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
+	"github.com/go-chi/chi/v5"
 	"github.com/timmy1496/social/internal/store"
 	"net/http"
+	"strconv"
 )
 
 type CreatePostPayload struct {
@@ -35,6 +38,36 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	postID, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+
+		return
+	}
+
+	ctx := r.Context()
+
+	post, err := app.storage.Posts.GetByID(ctx, postID)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 
 		return

5f16160fd1069c0d38de9f8f251acb7063a0e3c1
Artem Chernenko <artem.chernenko@betterme.world>
2025-05-02 18:41:27 +0300

    add comments to posts

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -73,6 +73,15 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	comments, err := app.storage.Comments.GetByPostID(ctx, postID)
+	if err != nil {
+		app.internalServerError(w, r, err)
+
+		return
+	}
+
+	post.Comments = comments
+
 	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 

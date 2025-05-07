97ee8541778639e284623e63e72a2cd37887cafe
Artem Chernenko <artem.chernenko@betterme.world>
2025-05-07 15:59:33 +0300

    add updating and deleting posts

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -76,3 +76,41 @@ func (s *PostStore) GetByID(ctx context.Context, postID int64) (*Post, error) {
 
 	return &post, nil
 }
+
+func (s *PostStore) Delete(ctx context.Context, postID int64) error {
+	query := `DELETE FROM posts WHERE id = $1`
+
+	res, err := s.db.ExecContext(ctx, query, postID)
+
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
+
+func (s *PostStore) Update(ctx context.Context, post *Post) error {
+	query := `
+		UPDATE posts
+		SET content = $1, title = $2
+		WHERE id = $3
+	`
+
+	_, err := s.db.ExecContext(ctx, query, post.Content, post.Title, post.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

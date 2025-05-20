43c0bec0a760e3ed65f319dee28c1cdcc15eba6e
Artem Chernenko <artem.chernenko@betterme.world>
2025-05-20 18:49:46 +0300

    feed filtering

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -25,7 +25,7 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 
 	ctx := r.Context()
 
-	feed, err := app.storage.Posts.GetUserFeed(ctx, int64(42), fq)
+	feed, err := app.storage.Posts.GetUserFeed(ctx, int64(12), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
